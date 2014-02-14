6805296cb07a7b20e1a3ca7403102e748e8ccbbe
boris yurkov <xiii1.3@yandex.ru>
2014-02-14 20:09:12 +0400

    LevelFactory, models

--- Assets/Scripts/Enum/Levels.js
+++ Assets/Scripts/Enum/Levels.js
@@ -3,5 +3,6 @@
 public static var Menu = "MainMenu";
 public static var Tutorial = "Tutorial";
 public static var Chat = "Chat";
+public static var LevelFactory = "LevelFactory";
 
-public static var Levels = [Chat];
+public static var Levels = [LevelFactory];
